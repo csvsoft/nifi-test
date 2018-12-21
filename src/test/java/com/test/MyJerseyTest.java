119e58ea283fc787bb518858f6fafcecc025c322
vzhang <ruquan.zhang@gmail.com>
2018-12-21 11:39:24 -0500

    Add processor listing

--- src/test/java/com/test/MyJerseyTest.java
+++ src/test/java/com/test/MyJerseyTest.java
@@ -1,16 +1,16 @@
 package com.test;
 
 
-
 import java.util.List;
+import java.util.UUID;
 
+import com.github.hermannpencole.nifi.swagger.client.ProcessorsApi;
+import com.github.hermannpencole.nifi.swagger.client.model.*;
 import org.junit.Test;
 
 import com.github.hermannpencole.nifi.swagger.ApiClient;
 import com.github.hermannpencole.nifi.swagger.ApiException;
 import com.github.hermannpencole.nifi.swagger.client.FlowApi;
-import com.github.hermannpencole.nifi.swagger.client.model.DocumentedTypeDTO;
-import com.github.hermannpencole.nifi.swagger.client.model.ProcessorTypesEntity;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -23,30 +23,87 @@ import javax.ws.rs.core.Response;
 //import org.glassfish.jersey.client.ClientConfig;
 
 public class MyJerseyTest {
-    
+
+    String baseURL = "http://35.231.236.230:8080/nifi-api";
     String url = "http://35.231.236.230:8080/nifi-api/flow/processor-types";
-    
-    @Test
-    public void test2(){
-        ApiClient apiClient = new ApiClient().setBasePath("http://35.231.236.230:8080/nifi-api");
-        FlowApi apiInstance = new FlowApi();
-        apiInstance.setApiClient(apiClient);
-String bundleGroupFilter = ""; // String | If specified, will only return types that are a member of this bundle group.
-String bundleArtifactFilter = ""; // String | If specified, will only return types that are a member of this bundle artifact.
-String type = ""; // String | If specified, will only return types whose fully qualified classname matches.
-try {
-    //apiInstance.getprocessorT
-    ProcessorTypesEntity result = apiInstance.getProcessorTypes(null, null, null);
-    List<DocumentedTypeDTO> list = result.getProcessorTypes();
-    for(DocumentedTypeDTO d:list){
-        System.out.println(d.getType());
-        
+    final String STATUS_RUNNING = "Running";
+    final String STATUS_STOPPED = "Stopped";
+
+    private void prt(Object obj){
+        System.out.println(obj);
     }
-    //System.out.println(result);
-} catch (ApiException e) {
-    System.err.println("Exception when calling FlowApi#getProcessorTypes");
-    e.printStackTrace();
-}
+    @Test
+    public void test2() {
+        ApiClient apiClient = new ApiClient().setBasePath("http://35.231.236.230:8080/nifi-api");
+        FlowApi flowApi = new FlowApi();
+        flowApi.setApiClient(apiClient);
+
+        ProcessorsApi procApi = new ProcessorsApi(apiClient);
+
+        String bundleGroupFilter = ""; // String | If specified, will only return types that are a member of this bundle group.
+        String bundleArtifactFilter = ""; // String | If specified, will only return types that are a member of this bundle artifact.
+        String type = ""; // String | If specified, will only return types whose fully qualified classname matches.
+        try {
+            //flowApi.getprocessorT
+            ProcessorTypesEntity result = flowApi.getProcessorTypes(null, null, null);
+            List<DocumentedTypeDTO> list = result.getProcessorTypes();
+            prt("Processor types:");
+            for (DocumentedTypeDTO d : list) {
+                System.out.println(d.getType());
+
+            }
+
+
+
+            ProcessGroupStatusEntity rootGrpStatus = flowApi.getProcessGroupStatus("root", true, true, null);
+            ProcessGroupStatusDTO processGroupStatus = rootGrpStatus.getProcessGroupStatus();
+            prt("group name:" + processGroupStatus.getName());
+            prt("group id:" + processGroupStatus.getId());
+
+            String rootGrpId = processGroupStatus.getId();
+
+            ProcessGroupFlowEntity nifi_flow = flowApi.getFlow("root");
+
+            List<ProcessorEntity> processors = nifi_flow.getProcessGroupFlow().getFlow().getProcessors();
+
+            for(ProcessorEntity p:processors){
+                ProcessorStatusDTO processorStatusDTO = p.getStatus();
+                prt(p.getComponent().getName() + " " + processorStatusDTO.getRunStatus());
+                ProcessorDTO processorDTO = p.getComponent();
+                ProcessorDTO.StateEnum processorState = processorDTO.getState();
+
+                if(processorState == ProcessorDTO.StateEnum.RUNNING){
+                    processorDTO.setState(ProcessorDTO.StateEnum.STOPPED);
+                }if(processorState == ProcessorDTO.StateEnum.STOPPED){
+                    processorDTO.setState(ProcessorDTO.StateEnum.RUNNING);
+                }
+                p.setComponent(processorDTO);
+
+
+                if(processorStatusDTO.getRunStatus().equalsIgnoreCase(STATUS_RUNNING)){
+                    processorStatusDTO.setRunStatus(STATUS_STOPPED);
+                }else if(processorStatusDTO.getRunStatus().equalsIgnoreCase(STATUS_STOPPED)){
+                    processorStatusDTO.setRunStatus(STATUS_RUNNING);
+                }else{
+                    prt("processor status NOT valid");
+                }
+
+                p.setStatus(processorStatusDTO);
+                RevisionDTO revisionDTO = p.getRevision();
+                revisionDTO.setClientId(UUID.randomUUID().toString());
+                p.setRevision(revisionDTO);
+                prt(p.toString());
+                procApi.updateProcessor(p.getId(),p);
+            }
+
+
+
+
+            //System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling FlowApi#getProcessorTypes");
+            e.printStackTrace();
+        }
     }
 
     //@Test
@@ -68,15 +125,15 @@ try {
 
         Client client = ClientBuilder.newClient();
         client.register(ProcessorTypesEntity.class);
-        
+
         WebTarget target = client.target(url);
 
 
-      ProcessorTypesEntity entity = target.request(MediaType.APPLICATION_JSON_TYPE).get(ProcessorTypesEntity.class);
+        ProcessorTypesEntity entity = target.request(MediaType.APPLICATION_JSON_TYPE).get(ProcessorTypesEntity.class);
 
         System.out.println(entity);
 
     }
 
-   
+
 }
